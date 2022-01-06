d7faa3e90a2b0daf93f4568e77f2889bdfa49386
Владислав,Прелин <crisebroun@gmail.com>
2022-01-06 16:25:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <img :src="require(`./assets/images/${images[value]}`)" :alt="value" />
+      <input type="range" v-model="value" :max="images.length - 1" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { images } from "./images";
+export default {
+  name: "app",
+  data() {
+    return {
+      value: 0,
+      images,
+    };
+  },
+};
+</script>
+<style>
+.wrapper {
+  display: inline-flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+img {
+  width: 350px;
+  height: 350px;
+}
+</style>
